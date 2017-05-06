84701a4191861b7aa69b817f08bd20f383e78522
kemar1997 <kemar.1997@outlook.com>
2017-05-06 12:31:50 -0400

    sets.py

---
+++ sets.py
@@ -0,0 +1,12 @@
+"""
+A set is a collection of items but, it can not have any duplicates like a list can. And also 
+a set is defined with curly braces "{}" instead of square brackets "[]".
+"""
+
+groceries = {'cereal', 'milk', 'starcrunch', 'beer', 'duct tape', 'lotion', 'beer'}
+# prints the list but only prints beer once so it is useless to add duplicates
+print(groceries)
+
+# gonna return true since indeed milk is in the groceries set
+if 'milk' in groceries:
+    
