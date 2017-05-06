87cc102fcb318028d7a5240b5e85b86687a0d3cf
kemar1997 <kemar.1997@outlook.com>
2017-05-06 11:36:36 -0400

    finished code

--- flexible_number_of_arguments.py
+++ flexible_number_of_arguments.py
@@ -2,8 +2,18 @@
 This is how you build a function that can take any amount of arguments.  What the * does is
 in the example below is it is going to be taking a bunch of arguments but it doesn't know 
 how many yet. So store them in the "args" variable... however many arguments there are. It 
-is common to always name the flexible arguments keyword "args".
+is common to always name the flexible arguments keyword "args". So the function below takes
+any amount of numbers when you call the function and adds them together and gives you an
+output. (Just like a calculator)
 """
 
 
 def add_numbers(*args):
+    total = 0
+    for a in args:
+        total += a
+    print(total)
+
+add_numbers(3)
+add_numbers(16, 22)
+add_numbers(1433, 2342, 3534, 3238, 6865)
