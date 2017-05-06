01dffa02d987955e437355b10ebea59d3fe1afee
kemar1997 <kemar.1997@outlook.com>
2017-05-06 11:16:10 -0400

    flexible_arguments

--- flexible_number_of_arguments.py
+++ flexible_number_of_arguments.py
@@ -1,7 +1,8 @@
 """
 This is how you build a function that can take any amount of arguments.  What the * does is
 in the example below is it is going to be taking a bunch of arguments but it doesn't know 
-how many yet. So store them in the "args" variable... however many arguments there are.
+how many yet. So store them in the "args" variable... however many arguments there are. It 
+is common to always name the flexible arguments keyword "args".
 """
 
 
