2f0040154785e26b93c4ef175824c1834796e714
kemar1997 <kemar.1997@outlook.com>
2017-05-06 13:13:42 -0400

    dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+"""
+A dictionary in python is a set of keys and then those keys are given values as if in RL when
+you have a dictionary you have a bunch of words and their definitions.
+"""
+
+classmates = {'Bucky':'is cool but smells', 'Emma':'sits behind me', 'Lucy':'asks too many questions'}
+
+# print(classmates)
+# print(classmates['Emma'])
+
+for k, v in classmates.items():
+    print(k + ' ' + v)
