bf78955c686d6c776c8e63c00ff6ab0512d0cc23
kemar1997 <kemar.1997@outlook.com>
2017-05-06 13:19:38 -0400

    dictionary.py edited

--- dictionary.py
+++ dictionary.py
@@ -3,7 +3,7 @@ A dictionary in python is a set of keys and then those keys are given values as 
 you have a dictionary you have a bunch of words and their definitions.
 """
 
-classmates = {'Bucky':'is cool but smells', 'Emma':'sits behind me', 'Lucy':'asks too many questions'}
+classmates = {'Bucky': ' is cool but smells', 'Emma': ' sits behind me', 'Lucy': ' asks too many questions'}
 
 # print(classmates)
 # print(classmates['Emma'])
