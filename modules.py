101d597ea210bb4d45c0fbd64290e9cd5f27cded
kemar1997 <kemar.1997@outlook.com>
2017-05-07 00:02:27 -0400

    edit modules.py and added the module example tuna.py

--- modules.py
+++ modules.py
@@ -3,5 +3,12 @@ Sometimes you are gonna have functions that you want to be used redundantly(over
 or maybe not only in the same program but in different programs. So instead of having to
 write the function over and over again, what you can do is put all the functions you want
 to reuse into its own separate file then include that file in whatever program you are 
-working on.
+working on. First thing you need to do is tell the program you want to include another file,
+in order to do that you use the 'import' keyword which basically tells the program or file
+to fetch another file to include with the original file. Then you have to tell import what
+file you want to use so in this case I wrote "import tuna", and to make a reminder... you 
+don't need to add the extension ".py" when importing because it already knows that it is 
+another python file unless it won't work, obviously.
 """
+
+import tuna
---
+++ tuna.py
@@ -0,0 +1,4 @@
+# simple function used an example for the modules.py
+def fish():
+    print("I am a tuna feesh!!" +
+          " And I am not the same as the others cause I am special.")
