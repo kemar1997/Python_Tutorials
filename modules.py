94f475fb9dc3230520851d05088e004098b5c7b3
kemar1997 <kemar.1997@outlook.com>
2017-05-06 23:12:24 -0400

    modules.py 1st commit

---
+++ modules.py
@@ -0,0 +1,7 @@
+"""
+Sometimes you are gonna have functions that you want to be used redundantly(over and over),
+or maybe not only in the same program but in different programs. So instead of having to
+write the function over and over again, what you can do is put all the functions you want
+to reuse into its own separate file then include that file in whatever program you are 
+working on.
+"""
