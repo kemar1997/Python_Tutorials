d3a03ac0deac75d5403cec2b36e86b4d555d5f47
kemar1997 <kemar.1997@outlook.com>
2017-05-09 01:52:32 -0400

    new additions

--- downloading_files_from_the_web.py
+++ downloading_files_from_the_web.py
@@ -1,6 +1,6 @@
 import csv
 
 with open('real_estate.csv', 'rU') as csvfile:
-    real_estate_reader = csv.reader(csvfile, delimiter=" ")
+    real_estate_reader = csv.reader(csvfile, delimiter=",")
     for row in real_estate_reader:
         print(' ,'.join(row))
