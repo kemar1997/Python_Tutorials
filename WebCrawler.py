25701f3d3c7833a934269ffb64b9b020f50283c9
kemar1997 <kemar.1997@outlook.com>
2017-05-10 01:17:08 -0400

    new additions

--- WebCrawler.py
+++ WebCrawler.py
@@ -13,10 +13,10 @@ def forum_spider(max_pages):
             title = link.string
             get_single_item_data(href)
             # print(href + " " + title)
-        for link in soup.findAll('a', {'class': 'user-name'}):
+        '''for link in soup.findAll('a', {'class': 'user-name'}):
             href = link.get('href')
             username = link.string
-            print(href + " " + username)
+            print(href + " " + username)'''
         page += 1
 
 
@@ -25,10 +25,10 @@ def get_single_item_data(item_url):
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, "html.parser")
     for item_name in soup.findAll('h1', {'class': 'forum-title'}):
-        '''print(item_name.string)'''
+        print(item_name.string)
     for link in soup.findAll('a'):
         href = link.get("href")
         print(href)
 
 
-forum_spider(5)
+forum_spider(1)
