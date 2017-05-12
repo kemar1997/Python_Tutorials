dfa9707033bc3123ac98b8f1a62a513cf820e5d4
kemar1997 <kemar.1997@outlook.com>
2017-05-12 07:41:06 -0400

    learned the exception keyword

---
+++ you_are_the_only_exception.py
@@ -0,0 +1,12 @@
+while True:
+    try:
+        number = int(input("What is your fav number?\n"))
+        result = 18/number
+        print(result)
+        break
+    except ValueError:
+        print("Error: Make sure to enter a real number.")
+    except ZeroDivisionError:
+        print("Choose any other number besides zero. Because logically a number divided by 0 is undefined.")
+    finally:
+        print("Loop complete")
