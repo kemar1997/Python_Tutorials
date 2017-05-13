e4e96ece09dbc174506ab2a0dea7ae3b11096358
Kemar Golding <kemar.1997@outlook.com>
2017-05-13 02:39:52 -0400

    inheritance.py change

--- inheritance.py
+++ inheritance.py
@@ -1,3 +1,9 @@
+"""
+Inheritance is basically copying functions from one class to another. Like in this example the Child class inherits
+the print_last_name function from the Parent class. 
+"""
+
+
 class Parent():
 
     def print_last_name(self):
